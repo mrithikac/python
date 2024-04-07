f47215275ab1d5d7da410993d97050370144f837
Mrithika <coomonmail77@gmail.com>
2024-04-07 09:22:20 -0700

    python code for variables

---
+++ app.py
@@ -0,0 +1,32 @@
+print("Hello, World!") #This is a comment.
+import sys
+print(sys.version)
+#print("python class")
+#This is a comment
+#They type of class is printed
+print ("python class")
+#Python Variables will be displayed.
+productname = "Pepsi"
+quantity= 10
+ratepercan= 1.50
+softdrink= True
+print(type(productname))
+ratepercan(type(ratepercan))
+bool 
+ratepercan: True #if it appears as float
+bool 
+ratepercan: False #if it appears in int
+print(ratepercan)
+my_variable_name = "Mrithika"
+print(my_variable_name) #this is the same as Mrithika.
+#We will display examples of Define and Global Variables
+i= "India is the"
+def myfunc():
+    global x 
+    global y
+    x= "5th largest economy"
+    y= "in the world"
+myfunc()
+print("After Germany," + i, x, y)
+# that is the same as After Germany, India is the 5th largest economy in the world.
+#With Varables Session 1 of Python tasks are DONE.
